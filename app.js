6cc70cefa329e79d5d54a5d3852caa0e51ef3cd6
grassfinn <theharper@live.com>
2023-01-03 21:44:23 -0500

    Basic Logic

--- app.js
+++ app.js
@@ -1,42 +1,57 @@
-// perhaps make this into a class or find out how to make it into a function
-
-const problem = {
-  number1: Math.floor(Math.random() * 10),
-  number2: Math.floor(Math.random() * 10),
-  operator: ['+', '-'],
-  generateQuestion: function question(num1, num2, opp) {
-    console.log(num1, opp, num2, ' = ', eval(`${num1} ${opp} ${num2}`));
-    return eval(`${num1} ${opp} ${num2}`);
-  },
-};
-const { number1, number2 } = problem;
-
 const operatorElement = document.querySelector('#operator');
+const userOperatorElement = document.querySelector('#user-operator');
 const num1Element = document.querySelector('#num1');
 const num2Element = document.querySelector('#num2');
 const solutionElement = document.querySelector('#solution');
+let hpElement = document.querySelector('.health');
+let pointsElement = document.querySelector('.points');
+let points = 0;
+let health = 100;
+
+function generateQuestion() {
+  const operator = ['+', '-'];
+  const randomOperator = operator[Math.floor(Math.random() * operator.length)];
+  const number1 = Math.floor(Math.random() * 10);
+  const number2 = Math.floor(Math.random() * 10);
+  const solution = eval(`${number1} ${randomOperator} ${number2}`);
+
+  console.log(
+    number1,
+    randomOperator,
+    number2,
+    ' = ',
+    eval(`${number1} ${randomOperator} ${number2}`)
+  );
+
+  num1Element.textContent = number1;
+  num2Element.textContent = number2;
+  operatorElement.textContent = randomOperator;
+  solutionElement.textContent = solution;
+  return eval(`${number1} ${randomOperator} ${number2}`);
+}
+hpElement.textContent = `Hp: ${health}`;
+pointsElement.textContent = `Points: ${points}`;
+
+generateQuestion();
+
 // const problemElement = document.querySelector('#problem');
-const randomOperator =
-  problem.operator[Math.floor(Math.random() * problem.operator.length)];
 
 function handleKeyPress(e) {
   // console.log(e);
   switch (e.key) {
     case 'ArrowUp':
-      operatorElement.textContent = '+';
-      // if (`${eval(num1Element.textContent + num2Element.textContent) === question(number1,number2, randomOperator)}`){
-      //   alert('Correct!')
-      // }
+      userOperatorElement.textContent = '+';
+      handleChange();
       break;
     case 'ArrowDown':
-      operatorElement.textContent = '-';
+      userOperatorElement.textContent = '-';
       handleChange();
       break;
     case 'ArrowLeft':
-      operatorElement.textContent = '*';
+      userOperatorElement.textContent = '*';
       break;
     case 'ArrowRight':
-      operatorElement.textContent = '/';
+      userOperatorElement.textContent = '/';
       break;
   }
 }
@@ -44,17 +59,30 @@ function handleKeyPress(e) {
 function handleChange() {
   // console.log(typeof operatorElement.textContent)
   // console.log(typeof randomOperator)
-  if (operatorElement.textContent != randomOperator) {
-    return;
+  if (userOperatorElement.textContent != operatorElement.textContent) {
+    console.log(
+      'incorrect!',
+      operatorElement.textContent,
+      userOperatorElement.textContent
+    );
+    health -= 5;
+    hpElement.textContent = `Hp: ${health}`;
+    return health;
   }
-  problem.generateQuestion(number1, number2, randomOperator);
+  console.log('correct!');
+  generateQuestion();
+  points++;
+  pointsElement.textContent = `Points: ${points}`;
+  return points;
 }
 
 // problemElement.textContent = `${number1} _ ${number2} = ${eval(question(number1, number1, randomOperator))}`
 
-num1Element.textContent = number1;
-num2Element.textContent = number2;
-solutionElement.textContent = problem.generateQuestion(number1, number2, randomOperator);
+// solutionElement.textContent = problem.generateQuestion(
+//   number1,
+//   number2,
+//   randomOperator
+// );
 // question(number1, number2, randomOperator);
 
 document.querySelector('body').addEventListener('keydown', handleKeyPress);
