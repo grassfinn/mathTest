6c563ddf71a7cc4fac8aa32cd9cf3a9be87ff1eb
grassfinn <theharper@live.com>
2023-01-04 22:08:53 -0500

    Styling,basic UI, and Replaced Evals

--- app.js
+++ app.js
@@ -17,26 +17,28 @@ let negCombo = 0;
 console.log(document.querySelector('.health-bar'));
 
 function generateQuestion() {
-  const operator = ['+', '-', '*', '/'];
+  const operator = ['+', '-'];
   const randomOperator = operator[Math.floor(Math.random() * operator.length)];
   // +1 prevents 0 shenanigans
   const number1 = Math.floor(Math.random() * 10) + 1;
   const number2 = Math.floor(Math.random() * 10) + 1;
-  const solution = eval(`${number1} ${randomOperator} ${number2}`);
 
-  console.log(
-    number1,
-    randomOperator,
-    number2,
-    ' = ',
-    eval(`${number1} ${randomOperator} ${number2}`)
-  );
+  // creating a function constructor that will run the code to get the solution
+  // https://www.youtube.com/watch?v=a7YGQ7kj6C0
+
+  const solution = (a, b, op) => {
+    return Function('a', 'b', `return (a ${op} b)`)(a, b);
+  };
+  // eval(`${number1} ${randomOperator} ${number2}`);
 
   num1Element.textContent = number1;
   num2Element.textContent = number2;
   operatorElement.textContent = randomOperator;
-  solutionElement.textContent = solution;
-  return eval(`${number1} ${randomOperator} ${number2}`);
+  solutionElement.textContent = solution(number1, number2, randomOperator);
+  // You can return a function that does the same thing that an eval would do
+  return function () {
+    `${number1} ${randomOperator} ${number2}`;
+  };
 }
 hpElement.textContent = `Hp: ${health}`;
 pointsElement.textContent = `Points: ${points}`;
@@ -46,7 +48,9 @@ generateQuestion();
 // const problemElement = document.querySelector('#problem');
 
 function handleKeyPress(e) {
-  // console.log(e);
+  // looking for the keys that have the data attribute and then console log
+  const keys = document.querySelector(`.arrows[data-key="${e.key}"]`);
+  console.log(keys);
   switch (e.key) {
     case 'ArrowUp':
       userOperatorElement.textContent = '+';
@@ -88,6 +92,8 @@ function handleChange() {
   negCombo = 0;
   return points;
 }
+
+
 
 document.querySelector('body').addEventListener('keydown', handleKeyPress);
 operatorElement.addEventListener('change', () => console.log('hi'));
